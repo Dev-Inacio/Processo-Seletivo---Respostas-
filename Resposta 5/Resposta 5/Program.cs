08fb7a83dfcb86481f261cc01b930f71ff3764a7
Dev-Inacio <143370692+Dev-Inacio@users.noreply.github.com>
2024-03-06 17:43:25 -0300

    Resposta 5

---
+++ Resposta 5/Resposta 5/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Digite algum nome para ser invertido: ");
+
+string PalavraNormal = Console.ReadLine();
+
+string PalavraInvertida = Inverter(PalavraNormal);
+Console.WriteLine(PalavraInvertida);
+
+static string Inverter(string text)
+{
+
+    char[] charArray = text.ToCharArray();
+
+    string Ivt = String.Empty;
+
+    for (int i = charArray.Length - 1; i >= 0; i--)
+    {
+        Ivt += charArray[i];
+    }
+
+    return Ivt;
+}
+
+
