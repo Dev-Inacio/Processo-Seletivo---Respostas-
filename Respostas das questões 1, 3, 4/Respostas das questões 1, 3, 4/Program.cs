931a4e0d2606d76828b9870844313bc31a89f082
Dev-Inacio <143370692+Dev-Inacio@users.noreply.github.com>
2024-03-06 17:50:11 -0300

    Respostas das questões 1,3,4.

---
+++ Respostas das questões 1, 3, 4/Respostas das questões 1, 3, 4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Resposta da questão: 1  -  91.
+
+
+// Resposta da questão: 3 - a) 9, b) 128, c) 49, d) 100, e) 13, f) 200.
+
+
+// Resposta da questão: 4 - ligaria um inrruptor e deixaria ligado por 20 minutos e verificaria e quando descobrisse
+// faria a mesma coisa com outro interruptor e quando descubrisse os dois o interruptor que faltasse seria a da terceira sala. 
+
+
