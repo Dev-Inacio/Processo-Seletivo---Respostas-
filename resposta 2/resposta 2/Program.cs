d8dec156c6d4a5aacaa864c65946af4ba0142461
Dev-Inacio <143370692+Dev-Inacio@users.noreply.github.com>
2024-03-06 17:32:43 -0300

    Resposta 2

---
+++ resposta 2/resposta 2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Verificador de numeros para sequência de Fibonacci: ");
+int numero = int.Parse(Console.ReadLine());
+
+static bool verificacao(int numero)
+{
+    int a = 0;
+    int b = 1;
+
+    while (b < numero)
+    {
+        int temp = a;
+        a = b;
+        b = temp + b;
+    }
+
+    return b == numero;
+}
+
+if (verificacao(numero))
+{
+    Console.WriteLine($"O numero {numero} pertence à sequência de Fibonacci.");
+}
+else
+{
+    Console.WriteLine($"O numero {numero} não pertence à sequência de Fibonacci.");
+}
